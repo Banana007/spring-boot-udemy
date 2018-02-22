75b3ed2a6237f0587d4b3b55506cfee02eb0034d
Evgenii Butkov <eugenefromrus@gmail.com>
2018-02-23 00:16:06 +1000

    First commit

---
+++ src/main/java/com/mycompany/springbootudemy/SpringBootUdemyApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.springbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUdemyApplication.class, args);
+	}
+}

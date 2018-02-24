5cd5038019183f3b7c5d667b20385cbf3fb33c76
Evgenii Butkov <eugenefromrus@gmail.com>
2018-02-24 22:06:37 +1000

    Added i18n localization with test

--- src/test/java/com/mycompany/springbootudemy/SpringBootUdemyApplicationTests.java
+++ src/test/java/com/mycompany/springbootudemy/SpringBootUdemyApplicationTests.java
@@ -1,16 +1,41 @@
 package com.mycompany.springbootudemy;
 
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mycompany.springbootudemy.web.i18n.I18Service;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class SpringBootUdemyApplicationTests {
 
+	@Autowired
+	private I18Service i18service;
+
+//	@Test
+//	public void contextLoads() {
+//	}
+
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception{
+		String excpectedResult  = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18service.getMessage(messageId);
+		Assert.assertEquals("The actual and expected String don't match", excpectedResult,actual);
 	}
 
 }

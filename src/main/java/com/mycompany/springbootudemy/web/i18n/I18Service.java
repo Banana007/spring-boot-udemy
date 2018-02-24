048155ceaaf33a71e781d0432217319df9af6e78
Evgenii Butkov <eugenefromrus@gmail.com>
2018-02-24 22:45:58 +1000

    Added loggin setup with logback

--- src/main/java/com/mycompany/springbootudemy/web/i18n/I18Service.java
+++ src/main/java/com/mycompany/springbootudemy/web/i18n/I18Service.java
@@ -1,5 +1,7 @@
 package com.mycompany.springbootudemy.web.i18n;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -9,10 +11,14 @@ import java.util.Locale;
 @Service
 public class I18Service {
 
+    /** The application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(I18Service.class);
+    
     @Autowired
     public MessageSource messageSource;
 
     public String getMessage(String messageId){
+        LOG.info("Returning i18n text for messageId {}",messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId,locale);
     }

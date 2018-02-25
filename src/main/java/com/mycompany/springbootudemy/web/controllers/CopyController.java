29b7479114d067022c551c7eec965d67703aaa68
Evgenii Butkov <eugenefromrus@gmail.com>
2018-02-26 00:57:49 +1000

    Added about us page

---
+++ src/main/java/com/mycompany/springbootudemy/web/controllers/CopyController.java
@@ -0,0 +1,14 @@
+package com.mycompany.springbootudemy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CopyController {
+
+    @GetMapping("/about")
+    public String about(){
+        return "copy/about";
+    }
+
+}

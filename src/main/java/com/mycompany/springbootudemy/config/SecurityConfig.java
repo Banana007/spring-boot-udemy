7c484522425ba4a957ce07c1503c56b8d8443200
Evgenii Butkov <eugenefromrus@gmail.com>
2018-03-06 01:05:36 +1000

    Added password encryption to String Security

--- src/main/java/com/mycompany/springbootudemy/config/SecurityConfig.java
+++ src/main/java/com/mycompany/springbootudemy/config/SecurityConfig.java
@@ -2,13 +2,16 @@ package com.mycompany.springbootudemy.config;
 
 import com.mycompany.springbootudemy.backend.service.UserSecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,6 +24,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Autowired
     private Environment env;
+
+    /**
+     * The encryption SALT.
+     */
+    private static final String SALT = "asfdsf$21123@!#safdase";
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12,new SecureRandom(SALT.getBytes()));
+    }
 
     /** Public URLs. */
     private static final String[] PUBLIC_MATCHERS = {
@@ -58,7 +71,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(userSecurityService);
+                .userDetailsService(userSecurityService)
+                .passwordEncoder(passwordEncoder());
     }
 
 }
